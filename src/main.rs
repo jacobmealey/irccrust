e09783b30f5dfb48f1e35aa1757122923fc50d47
Jacob Mealey <jacobmealey.maine@gmail.com>
2022-03-20 12:27:52 -0400

    comments comments comments

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
+// IRCrust: main.rs
+// Author: Jacob Mealey <jacob.mealey@maine.edu>
+// Main code for IRCrust (pronounced I-R-crust)
+// a "simple" IRC server written in rust because
+// I don't like being happy?
+
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 
 fn main() {
+    // bind to localhost:3030 how can we make the address and port
+    // constants or "predefine macros" like in C
     let listener = TcpListener::bind("localhost:3030").unwrap();
 
     for stream in listener.incoming() {
@@ -11,10 +19,15 @@ fn main() {
                 Ok(ref stream) => {
                     let num = handle_connection(&stream);
                     println!("Wrote {} bytes", num);
+                    // if zero, no bytes written connection is closed
+                    // (do we know that for sure?)
+                    // break out of 'loop' and scan for new connections
                     if num == 0 {
                         break;
                     }
                 }
+                // Not sure what type of errors there could be,
+                // so we'll find out if things break
                 Err(ref _e) => {println!("Error in stream :(");}
             }
         }
@@ -27,22 +40,28 @@ fn main() {
 // amount of bytes written to the stream. It reads a 
 // 1024 bytes at a time from the TcpStream
 fn handle_connection(mut stream: &TcpStream) -> usize {
+    // set buffer to size of 1024 and read from TcpStream 
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
     
     println!("Request from: {}", String::from_utf8_lossy(&buffer[..]));
 
+    // convert the input to uppercase
     let response = String::from_utf8_lossy(&buffer[..]).to_uppercase();
     
+    // set num (the return value)
     let num = response.len();
 
+    // need to match the wrte() to see if the error connection is still
+    // alive, not sure why we don't need to do it on the read (we probs should)
     match stream.write(response.as_bytes()) {
         Ok(_) => {
             stream.flush().unwrap();
             return num;
         }
+        // We should probably be checking what the error is and handling 
+        // instead of assuming the connection is dead.
         Err(_e) => {
-            //stream.shutdown(Shutdown::Both).expect("Shutdown call failed");
             println!("Connection Closed");
             return 0;
         }
