4142a995b5b08ee6ad07fa07feab75c5f9449782
Jacob Mealey <jacobmealey.maine@gmail.com>
2022-04-07 08:03:27 -0400

    reworked main.rs to use tokio for handling connections -- works as a tcp chat server

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,12 @@
 // I don't like being happy?
 
 use std::io::prelude::*;
-use std::net::TcpListener;
-use std::net::TcpStream;
 use std::thread;
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
 use std::sync::mpsc;
 use std::sync::mpsc::{Sender, Receiver};
+use tokio::{io::{AsyncWriteExt, BufReader, AsyncBufReadExt}, net::TcpListener, sync::broadcast};
 mod irc;
 
 const ADDR: &str = "localhost:3030";
@@ -33,86 +32,52 @@ impl Server<'_> {
     }
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     // bind to address ADDR 
-    let listener = match TcpListener::bind(ADDR) {
+    let listener = match TcpListener::bind(ADDR).await {
         Ok(listener) => listener,
         Err(e) => {panic!("Error binding to TCP socket: {}", e);}
     };
 
 
     let server_lock = Arc::new(Mutex::new(Server::new(String::from("localhost")))); 
-    let (tx, rx): (Sender<Server>, Receiver<Server>) = mpsc::channel();
 
-    let mut threads = vec![];
     
+    let (tx, _rx) = broadcast::channel(10);
 
     // currently we are only listening to a single connection at 
     // a time, we /should/ open a new thread everytime we get a 
     // connection
-    for stream in listener.incoming() {
-        // create a thread for each no connection. I don't really
-        // konw how to handle this properly but it doesn't seem
-        // terribel?
-        let server = Arc::clone(&server_lock);
-        let thread_tx = tx.clone();
-
-        let thread = thread::spawn(move || {
-            // we loop to ensure that stream stays in scope and 
-            // is not dropped (thus killing the connection)
-            let mut user: irc::User = irc::User {name: String::from("")};
+    loop {
+        let (mut socket, addr) = listener.accept().await.unwrap();
+        let mut line = String::new();
+        
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
+        
+        tokio::spawn(async move {
+            let (reader, mut writer) = socket.split();
+            let mut reader = BufReader::new(reader);
             loop {
-                let mut stream = match stream {
-                    Ok(ref stream) => stream,
-                    Err(_e) => {panic!("Error in stream :(");}
-                };
-                let mut buffer = [0; 1024];
-                stream.read(&mut buffer).unwrap();
+                tokio::select! {
+                    result = reader.read_line(&mut line) => {
+                        if result.unwrap() == 0 {
+                            break;
+                        }
 
-
-                let server = server.lock().unwrap();
-                let mut new_server = server.clone();
-                // search for first null character in array
-                let len = buffer.iter().position(|&p| p == 0).unwrap();
-
-                // slice index only to the length of the string
-                let client_in = String::from_utf8_lossy(&buffer[0..len]);
-                println!("client in: {}", client_in);
-                // decode client in 
-                let msgs = irc::commandf::message_decode(client_in.to_string());
-
-                let mut response: String = String::from("");
-                thread_tx.send(new_server);
-
-                println!("Response: \n{}", response);
-
-                // need to match the wrte() to see if the error connection is still
-                // alive, not sure why we don't need to do it on the read (we probs should)
-                match stream.write(response.as_bytes()) {
-                    Ok(_) => {
-                        stream.flush().unwrap();
-                        return len;
-                    }
-                    // We should probably be checking what the error is and handling 
-                    // instead of assuming the connection is dead.
-                    Err(_e) => {
-                        println!("Connection Closed");
-                        return 0;
+                        tx.send((line.clone(), addr)).unwrap();
+                    } result = rx.recv() => {
+                        let (msg, other_addr) = result.unwrap();
+                        if addr != other_addr {
+                            writer.write_all(msg.as_bytes()).await.unwrap();
+                        } 
                     }
                 }
-           }
-
+            }
         });
-
-        threads.push(thread);
     }
         
-    loop {
-        for msg in rx.recv() {
-            todo!();
-        }
-    }
-    
 }
 
 // Handle connection takes a TcpStream and returns the
@@ -121,43 +86,43 @@ fn main() {
 //
 // Ideally it should return a Result<> and have the err
 // handled properly
-fn _handle_connection(mut stream: &TcpStream, tx: &Sender<Server>, lock: &Arc<Mutex<Server>>, user: &mut irc::User) -> usize {
-    // set buffer to size of 1024 and read from TcpStream 
-    let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
-
-
-    //let server = Arc::clone(&lock);
-    let server = lock.lock().unwrap();
-
-    // search for first null character in array
-    let len = buffer.iter().position(|&p| p == 0).unwrap();
-
-    // slice index only to the length of the string
-    let client_in = String::from_utf8_lossy(&buffer[0..len]);
-    println!("client in: {}", client_in);
-    // decode client in 
-    let msgs = irc::commandf::message_decode(client_in.to_string());
-     
-    let mut response: String = String::from("");
-
-    
-    println!("Response: \n{}", response);
-    
-    // need to match the wrte() to see if the error connection is still
-    // alive, not sure why we don't need to do it on the read (we probs should)
-    match stream.write(response.as_bytes()) {
-        Ok(_) => {
-            stream.flush().unwrap();
-            return len;
-        }
-        // We should probably be checking what the error is and handling 
-        // instead of assuming the connection is dead.
-        Err(_e) => {
-            println!("Connection Closed");
-            return 0;
-        }
-    }
-    
-}
-
+// fn _handle_connection(mut stream: &TcpStream, tx: &Sender<Server>, lock: &Arc<Mutex<Server>>, user: &mut irc::User) -> usize {
+//     // set buffer to size of 1024 and read from TcpStream 
+//     let mut buffer = [0; 1024];
+//     stream.read(&mut buffer).unwrap();
+// 
+// 
+//     //let server = Arc::clone(&lock);
+//     let server = lock.lock().unwrap();
+// 
+//     // search for first null character in array
+//     let len = buffer.iter().position(|&p| p == 0).unwrap();
+// 
+//     // slice index only to the length of the string
+//     let client_in = String::from_utf8_lossy(&buffer[0..len]);
+//     println!("client in: {}", client_in);
+//     // decode client in 
+//     let msgs = irc::commandf::message_decode(client_in.to_string());
+//      
+//     let mut response: String = String::from("");
+// 
+//     
+//     println!("Response: \n{}", response);
+//     
+//     // need to match the wrte() to see if the error connection is still
+//     // alive, not sure why we don't need to do it on the read (we probs should)
+//     match stream.write(response.as_bytes()) {
+//         Ok(_) => {
+//             stream.flush().unwrap();
+//             return len;
+//         }
+//         // We should probably be checking what the error is and handling 
+//         // instead of assuming the connection is dead.
+//         Err(_e) => {
+//             println!("Connection Closed");
+//             return 0;
+//         }
+//     }
+//     
+// }
+// 
